db450fa76fdf9a02606cbea14c854d6fe6a8053e
yilin chai <yilin.chai@myer.com.au>
2025-08-19 16:38:35 +1000

    init

--- examples/react/tests/HelloWorld1.spec.tsx
+++ examples/react/tests/HelloWorld1.spec.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import HelloWorld from "../src/HelloWorld";
 
 test("renders name", async () => {
-  const { getByText, getByRole } = render(<HelloWorld name="Vitest" />);
+  const { getByText, getByRole } = render(<HelloWorld name="haha" />);
 
   await expect.element(getByText("Hello Vitest x1!")).toBeInTheDocument();
   await getByRole("button", { name: "Increment" }).click();
