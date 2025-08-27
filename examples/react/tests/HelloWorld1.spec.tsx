fc10b26c73516e75c6ff38cd27a64ae46d785271
yilin chai <chaiyilin@hotmail.com>
2025-08-28 06:15:46 +1000

    update

--- examples/react/tests/HelloWorld1.spec.tsx
+++ examples/react/tests/HelloWorld1.spec.tsx
@@ -2,11 +2,11 @@ import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import HelloWorld from "../src/HelloWorld";
 
-test("renders name", async () => {
+test("renders name1", async () => {
   const { getByText, getByRole } = render(<HelloWorld name="haha" />);
 
-  await expect.element(getByText("Hello Vitest x1!")).toBeInTheDocument();
+  await expect.element(getByText("Hello haha x1!")).toBeInTheDocument();
   await getByRole("button", { name: "Increment" }).click();
 
-  await expect.element(getByText("Hello Vitest x2!")).toBeInTheDocument();
+  await expect.element(getByText("Hello haha x2!")).toBeInTheDocument();
 });
